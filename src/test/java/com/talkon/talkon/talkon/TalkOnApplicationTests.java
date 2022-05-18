1daf5d370ea8c4cefdff8135fe4d82335644acd8
abdumajid2001 <abdumajidabdullatipov1@gmail.com>
2022-05-18 16:24:35 +0500

    initial commit

---
+++ src/test/java/com/talkon/talkon/talkon/TalkOnApplicationTests.java
@@ -0,0 +1,13 @@
+package com..talkon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalkOnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
