dffacc181f765cbff40ee720db04ac2b177ed1fb
Bobur Erkinov <boburerkinov002@gmail.com>
2022-05-21 16:05:28 +0500

    bobur schema

---
+++ src/main/java/com/talkon/talkon/dtos/mentor/MentorCreateDto.java
@@ -0,0 +1,7 @@
+package com.talkon.talkon.dtos.mentor;
+
+import com.talkon.talkon.dtos.base.BaseGenericDto;
+import com.talkon.talkon.dtos.base.GenericDto;
+
+public class MentorCreateDto implements BaseGenericDto {
+}
---
+++ src/main/java/com/talkon/talkon/dtos/mentor/MentorUpdateDto.java
@@ -0,0 +1,6 @@
+package com.talkon.talkon.dtos.mentor;
+
+import com.talkon.talkon.dtos.base.GenericDto;
+
+public class MentorUpdateDto extends GenericDto {
+}
